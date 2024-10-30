9b2273d83f62d8874024bb6c1f70efd043d41762
Daniel Vargas <50029247+danielvargass97@users.noreply.github.com>
2024-10-30 15:57:09 -0500

    Initial controllers

---
+++ src/main/java/com/armirene/employees/controller/EmployeesController.java
@@ -0,0 +1,8 @@
+package com.armirene.employees.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/employees")
+public class EmployeesController {
+
+}
